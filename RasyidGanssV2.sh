13c3a8955d2c8dabd8b2534a882e8ba10b2e19b3
Mr-004 <57210483+Mr-004@users.noreply.github.com>
2019-12-29 14:51:05 +0700

    Add files via upload

---
+++ RasyidGanssV2.sh
@@ -0,0 +1,65 @@
+#MAU NGAPAIN MONYET !!
+#MAU RECODE !?
+#HAHAHA KONTOL RECODE AJG !!
+#NGAPAIN MASIH DI SINI MONYET !!
+#PERGI LAGI SANA KONTOL !!
+#BANGSAT LU ANJING !!
+#LU YANG MONYET !!
+#PERGI JAUH JAUH KONTOL BABI !!
+#SALAM HEKEL
+#SALAM MAQLO
+clear
+python2 meizu.py
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+echo "##################################"|lolcat
+echo "/ TOOLS : SPAM v1                /"|lolcat
+echo "/ CODED : MR.004               /"|lolcat
+echo "/ TEAM  : 004 CYEBER TEAM  /"|lolcat
+echo "/ youtube : Gamer 4 LIve           /"|lolcat
+echo "##################################"|lolcat
+
+echo "Daftar Tools spam"
+echo "[1] Bom Chat Whatsapp"
+echo "[2] Spam Telpon"
+echo "[3] Keluar Asw Dari Tols Gwe"
+read -p "PILIH SALAH SATU LONTE : " bro
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+php mdr2.php
+sh B3604-spam.sh
+fi
+
+if [ $bro = 2 ] || [ $bro = 01 ]
+then
+clear
+php mdr1.php
+sh B3604-spam.sh
+fi
+
+if [ $bro = 3 ] || [ $bro = 03 ]
+then
+echo "\033[31;1mWE ARE 004 CYEBER TEAM"
+sleep 1
+echo "\033[32;1mTerimakasih Sudah Menggunakan Tools ini"
+sleep 1
+echo "\033[35;1mSelamat Coly"
+sleep 1
+exit
+fi
